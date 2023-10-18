86837441a1d5dc65a791be88f8121ec580bc329a
Luca Rodrigues <luca.rodrigues@wundermanthompson.com>
2023-10-18 15:20:05 -0300

    feat: category mapper e container/presentation pattern para edit contact

---
+++ src/pages/EditContact/Container.js
@@ -0,0 +1,70 @@
+import { useState, useEffect, useRef } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
+import useSafeAsyncAction from '../../hooks/useSafeAsyncAction';
+
+import Presentation from './Presentation';
+
+export default function Container() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+  const runSafeAsyncAction = useSafeAsyncAction();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        runSafeAsyncAction(() => {
+          contactFormRef.current.setFieldsValues(contact);
+          setIsLoading(false);
+          setContactName(contact.name);
+        });
+      } catch {
+        runSafeAsyncAction(() => {
+          history.push('/');
+          toast({
+            type: 'danger',
+            text: 'Contato não encontrado',
+          });
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history, runSafeAsyncAction]);
+
+  async function handleSubmit(contact) {
+    try {
+      const contactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(contactData.name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch (error) {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
+  }
+
+  return (
+    <Presentation
+      isLoading={isLoading}
+      contactName={contactName}
+      contactFormRef={contactFormRef}
+      onSubmit={handleSubmit}
+    />
+  );
+}
